d6334588fcd61b0bbf62ef21047cf8ae463ebc9b
Nisrin-prog <69914755+Nisrin-prog@users.noreply.github.com>
2021-06-24 12:10:58 +0530

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -1,18 +1,17 @@
 class Umbrella {
     constructor(x,y){
         var options = {
-            'isStatic': true,
+            isStatic: true,
         }
-        this.body = Bodies.circle(x,y,50,options);
+        this.image = loadImage("Walking Frame/walking_1.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
         this.radius = 50;
-        this.image = loadImage("images/Walking Frame/walking_1.png","images/Walking Frame/walking_2.png","images/Walking Frame/walking_3.png","images/Walking Frame/walking_4.png","images/Walking Frame/walking_5.png","images/Walking Frame/walking_6.png","images/Walking Frame/walking_7.png","images/Walking Frame/walking_8.png");
-        World.add(world, this.body)
+        World.add(world, this.umbrella)
     }
 
     display(){
-        var pos = this.body.position;
+        var pos = this.umbrella.position;
         imageMode(CENTER);
-        image(this.image, pos.x, pos.y+70, 300, 300);
+        image(this.image,pos.x,pos.y+70,300,300);
     }
 }
-
